be28dc3ca4aa9902005716de4f4b506e0e8cbd70
boulavogue <meirbhin@gmail.com>
2017-09-22 15:00:58 +1000

    Update Dynamic_ExecAll_StoredProc.sql

--- Dynamic_ExecAll_StoredProc.sql
+++ Dynamic_ExecAll_StoredProc.sql
@@ -1,9 +1,13 @@
 /* EXECUTES ALL STORED PROCEDURES */
+
+
 USE [DatabaseName]
 declare @SQL nvarchar(max)
 
 SELECT @SQL = STUFF((SELECT '; EXEC ' + quotename(SPECIFIC_SCHEMA) + '.' + quotename(SPECIFIC_NAME) 
 FROM INFORMATION_SCHEMA.Routines WHERE Routine_Type = 'Procedure'
+--AND ISNUMERIC(LEFT(Routine_Name,1)) = 1 /* Include only SPs starting with a number */
+--AND LEFT(Routine_Name,1) > '0' /* Exclude this SP - Otherwise we'd have a loop */
 --AND Routine_Name NOT LIKE '%_old'
 FOR XML PATH('')),1,2,'')
 
